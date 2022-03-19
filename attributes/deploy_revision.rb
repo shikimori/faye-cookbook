4321a71fb964e068daa61b0befc074facee6f203
Andrey Sidorov <takandar@gmail.com>
2022-03-19 21:34:55 +0300

    fix git url

--- attributes/deploy_revision.rb
+++ attributes/deploy_revision.rb
@@ -2,5 +2,5 @@ default['faye']['deploy_user'] = 'root'
 default['faye']['deploy_group'] = 'users'
 default['faye']['deploy_migrate'] = false
 default['faye']['deploy_action'] = 'deploy'
-default['faye']['repo'] = 'git://github.com/shikimori/faye-server.git'
+default['faye']['repo'] = 'https://github.com/shikimori/faye-server.git'
 default['faye']['branch'] = 'master'
